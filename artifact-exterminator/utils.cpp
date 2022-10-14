f2a432ca62c36583e14b32f5200938a3b83d4d65
George <george.neokq@gmail.com>
2022-10-14 16:27:59 +0800

    refactor: changes tabs to spaces

--- artifact-exterminator/utils.cpp
+++ artifact-exterminator/utils.cpp
@@ -6,18 +6,18 @@ BOOL SetPrivilege(
     HANDLE hToken,          // access token handle
     LPCTSTR lpszPrivilege,  // name of privilege to enable/disable
     BOOL bEnablePrivilege   // to enable or disable privilege
-    ) 
+)
 {
     TOKEN_PRIVILEGES tp;
     LUID luid;
 
-    if ( !LookupPrivilegeValue( 
-            NULL,            // lookup privilege on local system
-            lpszPrivilege,   // privilege to lookup 
-            &luid ) )        // receives LUID of privilege
+    if (!LookupPrivilegeValue(
+        NULL,            // lookup privilege on local system
+        lpszPrivilege,   // privilege to lookup 
+        &luid))        // receives LUID of privilege
     {
-       wprintf(L"LookupPrivilegeValue error: %u\n", GetLastError() ); 
-        return FALSE; 
+        wprintf(L"LookupPrivilegeValue error: %u\n", GetLastError());
+        return FALSE;
     }
 
     tp.PrivilegeCount = 1;
@@ -29,23 +29,23 @@ BOOL SetPrivilege(
 
     // Enable the privilege or disable all privileges.
 
-    if ( !AdjustTokenPrivileges(
-           hToken, 
-           FALSE, 
-           &tp, 
-           sizeof(TOKEN_PRIVILEGES), 
-           (PTOKEN_PRIVILEGES) NULL, 
-           (PDWORD) NULL) )
-    { 
-          printf("AdjustTokenPrivileges error: %u\n", GetLastError() ); 
-          return FALSE; 
-    } 
+    if (!AdjustTokenPrivileges(
+        hToken,
+        FALSE,
+        &tp,
+        sizeof(TOKEN_PRIVILEGES),
+        (PTOKEN_PRIVILEGES)NULL,
+        (PDWORD)NULL))
+    {
+        printf("AdjustTokenPrivileges error: %u\n", GetLastError());
+        return FALSE;
+    }
 
     if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
     {
-          printf("The token does not have the specified privilege. \n");
-          return FALSE;
-    } 
+        printf("The token does not have the specified privilege. \n");
+        return FALSE;
+    }
 
     return TRUE;
 }

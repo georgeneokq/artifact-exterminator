189f0a80eaf40a6e58fb5ede66a88e4be5dfefe4
George <george.neokq@gmail.com>
2022-10-18 22:53:03 +0800

    docs: Update arguments documentation in main.cpp

--- artifact-exterminator/main.cpp
+++ artifact-exterminator/main.cpp
@@ -26,19 +26,20 @@ int wmain(int argc, wchar_t* argv[])
     /* 
      * Argument list. Values should come after their flags, separated by spaces.
      * e.g. -f C:\Windows\System32\executable.exe
-     * -f File path of executable
-     * --args Arguments for specified executable
-     * --killswitch-ip IPv4 address of kill switch socket
-     * --killswitch-port Port of remote socket
-     * --killswitch-poll Interval for polling, in seconds. Defaults to once every 10 seconds.
+     * -f File path of executable. If the executable can be found in PATH, there is no need to specify the full path.
+     *    Alternatively, this argument can be any command line tool, as it is ran in the context of a shell, using cmd /c
+     * --args Arguments for executable specified via -f
+     * --killswitch-ip IPv4 address of kill switch socket. Must be provided along with killswitch-port option
+     * --killswitch-port Port of remote socket. Must be provided along with killswitch-ip option
+     * --killswitch-poll Interval for polling, in seconds. This argument is optional; defaults to once every 10 seconds
      * -k Registry keys to remove, comma-separated
      * -v Registry values to remove, comma-separated. Value name should come after the key, separated by colon
      *    e.g. HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\AppCompatCache:AppCompatCache
-     *    The root key can be specified by either its full name or by its shorthand
-     *    i.e. HKEY_LOCAL_MACHINE HKLM
-     * -a Additional file paths to clean up
+     *    The root key can be specified by either its full name or by its shorthand, like HKLM
+     * -a Additional file names to remove traces of, comma-separated
      * -s Only run shimcache removal function. The value of this option is not relevant, but is still required.
-     *    e.g. artifact-exterminator.exe -f C:\Windows\System32\executable.exe -s 1
+     *    e.g. artifact-exterminator.exe -s 1 -a calc.exe,mimikatz.exe.
+     *    This argument is mainly for internal use within the program code for scheduling tasks to clear shimcache upon system reboot.
      */
 
     // Parse command line arguments

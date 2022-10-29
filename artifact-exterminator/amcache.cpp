c4eb81c5859502ce4de05b8696655be2578ea41f
Samsterr <61813561+Samsterr@users.noreply.github.com>
2022-10-29 14:06:55 +0800

    Added Enumeration and Deletion of Key
    Weird Race condition where it connects registry while it is being imported

--- artifact-exterminator/amcache.cpp
+++ artifact-exterminator/amcache.cpp
@@ -1,7 +1,29 @@
 #include "amcache.h"
 
+#define MAX_KEY_LENGTH 255
+#define MAX_VALUE_NAME 16383
+
+//Query Key Function from Microsoft https://learn.microsoft.com/en-us/windows/win32/sysinfo/enumerating-registry-subkeys
 void removeAmcache(wchar_t* executableName)
 {
+    TCHAR    achKey[MAX_KEY_LENGTH];   // buffer for subkey name
+    DWORD    cbName;                   // size of name string 
+    TCHAR    achClass[MAX_PATH] = TEXT("");  // buffer for class name 
+    DWORD    cchClassName = MAX_PATH;  // size of class string 
+    DWORD    cSubKeys = 0;               // number of subkeys 
+    DWORD    cbMaxSubKey;              // longest subkey size 
+    DWORD    cchMaxClass;              // longest class string 
+    DWORD    cValues;              // number of values for key 
+    DWORD    cchMaxValue;          // longest value name 
+    DWORD    cbMaxValueData;       // longest value data 
+    DWORD    cbSecurityDescriptor; // size of security descriptor 
+    FILETIME ftLastWriteTime;      // last write time 
+
+    DWORD i, retCode;
+
+    TCHAR  achValue[MAX_VALUE_NAME];
+    DWORD cchValue = MAX_VALUE_NAME;
+
     // Deletes the associated Amcache entry
     STARTUPINFOW si;
     ZeroMemory(&si, sizeof(si));
@@ -17,9 +39,8 @@ void removeAmcache(wchar_t* executableName)
     CloseHandle(pi.hProcess);
 
     HKEY hKey;
-    DWORD numKeys = 0;
     RegOpenKeyW(HKEY_LOCAL_MACHINE, L"AM\\Root\\InventoryApplicationFile", &hKey);
-	RegQueryInfoKey(hKey, NULL, NULL, NULL, &numKeys, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
 
     // Amcache max length of exe name in keys is 16 and all in lowercase
     wchar_t executableNameTruncated[17];
@@ -29,29 +50,45 @@ void removeAmcache(wchar_t* executableName)
     wprintf(L"[DEBUG] Amcache key extracted: %s\n", executableNameTruncated);
 
     // Enumerate through all subkeys. If the truncated executable name is a part of the key name, delete it
-    wchar_t keyName[256];
-    int index = 0;
-    DWORD cchName = 256;
-    for(int i = 0; i < numKeys; i++)
+    // Get the class name and the value count. 
+    RegQueryInfoKey(
+        hKey,                    // key handle 
+        achClass,                // buffer for class name 
+        &cchClassName,           // size of class string 
+        NULL,                    // reserved 
+        &cSubKeys,               // number of subkeys 
+        &cbMaxSubKey,            // longest subkey size 
+        &cchMaxClass,            // longest class string 
+        &cValues,                // number of values for this key 
+        &cchMaxValue,            // longest value name 
+        &cbMaxValueData,         // longest value data 
+        &cbSecurityDescriptor,   // security descriptor 
+        &ftLastWriteTime);       // last write time
+
+    wprintf(L"DEBUG: KEY NUM %d\n", cSubKeys);
+    if (cSubKeys)
     {
-        RegEnumKeyExW(hKey, index, keyName, &cchName, NULL, NULL, NULL, NULL);
-
-        wprintf(L"[DEBUG] Enumerating key: %s\n", keyName);
-
-        wchar_t* substrPtr = wcsstr(keyName, executableNameTruncated);
-
-        // Found the key to delete!
-        if (substrPtr != NULL)
+        for (i = 0; i < cSubKeys; i++)
         {
-            wprintf(L"[DEBUG] Deleting key: %s\n", keyName);
-            int err = RegDeleteKeyW(hKey, keyName);
-            wprintf(L"[DEBUG] Error for deleting key in amcache: %d\n", err);
+            cbName = MAX_KEY_LENGTH;
+            retCode = RegEnumKeyEx(hKey, i,achKey,&cbName,NULL,NULL,NULL,&ftLastWriteTime);
+            wprintf(L"DEBUG: Current Key Name %s\n", achKey);
 
-            if (err == ERROR_SUCCESS)
-                wprintf(L"[DEBUG] Successfully deleted key: %s\n", keyName);
-
-            break;
+            if(retCode == ERROR_SUCCESS)
+            {
+                wchar_t* substrPtr = wcsstr(achKey, executableNameTruncated);
+                if (substrPtr != NULL)
+                {
+                    wprintf(L"DEBUG : Key Has been Found!\n");
+                    retCode = RegDeleteKeyW(hKey, achKey);
+                    if (retCode == ERROR_SUCCESS)
+                    {
+                        wprintf(L"Successfully deleted Key!\n");
+                        RegCloseKey(hKey);
+                    }
+                    break;
+                }
+            }
         }
-        index++;
     }
 }
